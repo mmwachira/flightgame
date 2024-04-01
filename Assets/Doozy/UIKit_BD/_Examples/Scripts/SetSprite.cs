2c11cccb7927537e0a8a36a6bd808b8393601700
Leandro_Gil <gil.leandro.o@gmail.com>
2024-04-01 13:39:33 +0300

    Added UI dependencies (DoozyUI, Quick) Added "UI Sample" scene Added "HUD.prefab"

---
+++ Assets/Doozy/UIKit_BD/_Examples/Scripts/SetSprite.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class SetSprite : MonoBehaviour {
+
+	void Start () 
+	{
+		//string[] goName = gameObject.name.Split('_');
+		GetComponent<Image>().sprite = Resources.Load <Sprite> ("Sprites/Ribbons/@1x/"+gameObject.name);
+	}
+	
+}
