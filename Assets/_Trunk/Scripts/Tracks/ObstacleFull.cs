beee93774ab4ad0a59fa0a64a511deb3f4e070d9
Leandro_Gil <gil.leandro.o@gmail.com>
2024-05-09 15:09:08 +0300

    Added third type of obstacles

---
+++ Assets/_Trunk/Scripts/Tracks/ObstacleFull.cs
@@ -0,0 +1,9 @@
+﻿public class ObstacleFull : Obstacle
+{
+    public override void Spawn(TrackSegment segment, float t)
+    {
+        segment.GetPointAt(t, out var position, out var rotation);
+
+        Instantiate(gameObject, position, rotation, segment.ContainerObstacles);
+    }
+}
